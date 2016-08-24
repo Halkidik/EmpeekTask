4875d8fb02d25b2c9591ff68292e3410007c9843
Halkidik <farnakov.petr96@gmail.com>
2016-08-25 00:16:58 +0300

    Project

---
+++ EmpeekTask/Scripts/APIScripts/Service.js
@@ -0,0 +1,29 @@
+﻿app.service("APIService", function ($http,$q) {
+    this.getInformation = function () {
+        var url = 'api/Home';
+        return $http.get(url).then(function (response) {
+            return response.data;
+        });
+    }
+    this.getFiles = function (path) {
+        return $http({
+            method: 'post',
+            data: path,
+            url: 'api/Home'
+        });
+    }
+    var deferredAbort = $q.defer();
+    this.getCount = function () {
+        return $http({
+            method: 'get',
+            url: 'api/Home/Counter',
+            timeout: deferredAbort.promise
+        });
+    }
+    this.abort = function(){
+        deferredAbort.resolve();
+        deferredAbort = $q.defer();
+    }
+
+});
+
